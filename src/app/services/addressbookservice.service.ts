8e161966511ddf8bf08665d039efe0513d343b65
Sippora04 <sippora.toppo95@gmail.com>
2021-01-10 21:22:52 +0530

    Added: address book frontend

---
+++ src/app/services/addressbookservice.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddressbookserviceService {
+
+  constructor(private http: HttpClient) { }
+
+  createContact(data: any) {
+    return this.http.post('http://localhost:8080/addressbook/create', data);
+  }
+
+  getEmployee(id: any) {
+    return this.http.get('http://localhost:8080/addressbook/get/' + id);
+  }
+
+  getEmployeeList() {
+    return this.http.get('http://localhost:8080/addressbook/get');
+  }
+
+  deleteEmployee(id: any) {
+    return this.http.delete('http://localhost:8080/addressbook/delete/' + id);
+  }
+
+  updateEmployee(data: any, id: any) {
+    return this.http.put('http://localhost:8080/addressbook/update/' + id, data);
+  }
+}
